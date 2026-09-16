655e6041afd6099a86f27ef2e3732ad991f0838a
Ye Min Thu <245580708+ymt-nk40@users.noreply.github.com>
2026-09-16 20:03:00 +0630

    package.json fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +33,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      {children}
+      <SpeedInsights />
+      </body>
     </html>
   );
 }
